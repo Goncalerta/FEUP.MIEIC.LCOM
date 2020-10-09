1d649e4f4f5b3c9f8d5442056dfda7ea77aa54f3
Pedro Goncalo Correia <up201905348@feup-b301d27.fe.up.pt>
2020-10-09 10:35:25 +0100

    Implement tmer_test_read_config

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -30,10 +30,11 @@ int main(int argc, char *argv[]) {
 }
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint8_t config;
 
-  return 1;
+  // Short-circuits if first function call fails
+  return timer_get_conf(timer, &config) 
+    || timer_display_conf(timer, config, field);
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
