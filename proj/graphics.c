5d38b54665fb9a5e41bca583242039287bf6279f
Pedro Goncalo Correia <up201905348@fe.up.pt>
2020-11-28 19:14:08 +0000

    Revert width drawing because it is inefficient

--- proj/graphics.c
+++ proj/graphics.c
@@ -101,6 +101,7 @@ int vb_draw_circle(video_buffer_t buf, uint16_t x, uint16_t y, uint16_t radius, 
 }
 
 int vb_draw_line(video_buffer_t buf, int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint32_t color, uint16_t width) {
+    // TODO implement width
     if (y1 == y2) {
         if (x2 < x1) {
             int16_t temp = x2;
@@ -108,7 +109,10 @@ int vb_draw_line(video_buffer_t buf, int16_t x1, int16_t y1, int16_t x2, int16_t
             x1 = temp;
         }
         for (uint16_t x = x1; x <= x2; x++) {
-            if (vb_draw_circle(buf, x, y1, width, color) != OK) {
+            // if (vb_draw_circle(buf, x, y1, width, color) != OK) {
+            //     return 1;
+            // }
+            if (vb_draw_pixel(buf, x, y1, color) != OK) {
                 return 1;
             }
         }
@@ -121,22 +125,38 @@ int vb_draw_line(video_buffer_t buf, int16_t x1, int16_t y1, int16_t x2, int16_t
             y1 = temp;
         }
         for (uint16_t y = y1; y <= y2; y++) {
-            if (vb_draw_circle(buf, x1, y, width, color) != OK) {
+            // if (vb_draw_circle(buf, x1, y, width, color) != OK) {
+            //     return 1;
+            // }
+            if (vb_draw_pixel(buf, x1, y, color) != OK) {
                 return 1;
             }
         }
         return 0;
     }
 
-    int dx =  abs(x2-x1), sx = x1<x2 ? 1 : -1;
-    int dy = -abs(y2-y1), sy = y1<y2 ? 1 : -1; 
-    int err = dx+dy, e2;
+    int dx = abs(x2-x1); 
+    int sx = x1 < x2 ? 1 : -1;
+    int dy = abs(y2-y1); 
+    int sy = y1 < y2 ? 1 : -1; 
+    int err = dx - dy, e2;
  
     while (x1 != x2 || y1 != y2) {
-        vb_draw_circle(buf, x1, y1, width, color);
+        // if (vb_draw_circle(buf, x1, y1, width, color) != OK) {
+        //     return 1;
+        // }
+        if (vb_draw_pixel(buf, x1, y1, color) != OK) {
+            return 1;
+        }
         e2 = 2*err;
-        if (e2 >= dy) { err += dy; x1 += sx; }
-        if (e2 <= dx) { err += dx; y1 += sy; }
+        if (e2 >= -dy) { 
+            err -= dy; 
+            x1 += sx; 
+        }
+        if (e2 <= dx) { 
+            err += dx; 
+            y1 += sy; 
+        }
     }
 
     return 0;
